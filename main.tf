2a03eb8b925c7a3f698ea295135ae2275b4acaa4
aursalva1 <aursalva1@gmail.com>
2024-10-03 21:03:03 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "aws_vpc" "eks_vpc" {
 resource "aws_subnet" "eks_subnet" {
   count = 2
   vpc_id = aws_vpc.eks_vpc.id
-  cidr_block = cidrsubnet(var.vpc_cidr, 20, count.index)  # Usar /20
+  cidr_block = cidrsubnet(var.vpc_cidr, 8, count.index)  # Usar /24
   availability_zone = element(data.aws_availability_zones.available.names, count.index)
   tags = {
     Name = "eks-subnet-${count.index}"
