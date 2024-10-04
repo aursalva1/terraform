6247b4acbeaa286ccc81df927da6d173899d7da5
aursalva1 <aursalva1@gmail.com>
2024-10-03 20:23:58 -0600

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,10 @@
-output "eks_cluster_name" {
-  value = module.eks.cluster_id
+# outputs.tf
+output "cluster_endpoint" {
+  description = "URL del endpoint de EKS"
+  value       = aws_eks_cluster.eks_cluster.endpoint
 }
 
-output "eks_cluster_endpoint" {
-  value = module.eks.cluster_endpoint
+output "cluster_name" {
+  description = "Nombre del clúster EKS"
+  value       = aws_eks_cluster.eks_cluster.name
 }
