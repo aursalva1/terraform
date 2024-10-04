c403e91e52b71221ca5c4b55f1d91662e36477f4
aursalva1 <aursalva1@gmail.com>
2024-10-03 20:24:47 -0600

    Create versions.tf

---
+++ versions.tf
@@ -0,0 +1,14 @@
+# versions.tf
+terraform {
+  required_version = ">= 0.12"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+  }
+}
