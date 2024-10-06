a5283b14dfe10ccfcb20aa190ee15de8a474c9b7
aursalva1 <aursalva1@gmail.com>
2024-10-05 18:42:57 -0600

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,14 +1,4 @@
 # versions.tf
 terraform {
   required_version = ">= 0.12"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.0"
-    }
-  }
 }
